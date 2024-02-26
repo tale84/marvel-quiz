798146eefcdb50213575bc2762185e5548d6e4ae
tuananhle <dtuananh.le@gmail.com>
2024-02-26 15:45:04 +0100

    add button and routes

---
+++ src/components/Signup/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className="signUpLoginBox">
+        <div className="slContainer">
+            Sign up
+        </div>
+    </div>
+  )
+}
+
+export default Signup
