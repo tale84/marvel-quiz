95c76cffd7d6495cf8c8f1778f0639ff5e933028
tuananhle <dtuananh.le@gmail.com>
2024-02-26 15:45:04 +0100

    add button and routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,14 +2,27 @@ import '../../App.css';
 import Header from "../Header"
 import Landing from "../Landing"
 import Footer from "../Footer"
+import Welcome from '../Welcome';
+import Login from '../Login';
+import Signup from '../Signup';
+import ErrorPage from '../ErrorPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Landing />
+
+      <Routes>
+        <Route path="/" exact element={<Landing />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </Router>
   );
 }
 
