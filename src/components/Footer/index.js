9da6c9d4fec95af34122cd4f50b1783a0c18818c
tuananhle <dtuananh.le@gmail.com>
2024-02-26 11:00:54 +0100

    add header landing and footer to app

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <p>Projet réalisé par Tale - 2024<br />
+            Les icônes sont prises sur le site iconFinder.com</p>
+        </footer>
+    )
+}
+
+export default Footer
